3e8c6b49d2ca060c9866c7f154b7c90565c5cfd2
Sky Luo <skluo@davidson.edu>
2022-10-05 00:54:39 -0400

    updated log message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
 
 /* javascript */
 console.log("The original design is [the velvet underground at paramount, 1970] from https://www.swissted.com/");
+console.log("I am Sky Luo, recreator of the original design");
